52915b65a58e6feba01bf01c25cc9f008d905447
thatperturbedprogrammer <96286692+thatperturbedprogrammer@users.noreply.github.com>
2025-02-22 22:50:53 +0530

    Create prisma-singleton-pattern.ts

---
+++ prisma-singleton-pattern.ts
@@ -0,0 +1,15 @@
+prisma Singleton Pattern
+// src/lib/prisma.ts
+import { PrismaClient } from "@prisma/client";
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+export const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
