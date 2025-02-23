8f067b7694b9308825d3a93b1e2fb341740c99a5
thatperturbedprogrammer <96286692+thatperturbedprogrammer@users.noreply.github.com>
2025-02-23 09:51:35 +0530

    Update prisme-singleton-pattern.ts

--- Prisma Patterns/prisme-singleton-pattern.ts
+++ Prisma Patterns/prisme-singleton-pattern.ts
@@ -1,4 +1,9 @@
 // prisma Singleton Pattern
+
+// Why ?
+// Frameworks like Next.js support hot reloading of changed files, which enables you to see changes to your application without restarting. 
+// However, if the framework refreshes the module responsible for exporting PrismaClient, this can result in additional, unwanted instances of PrismaClient in a development environment.
+// As a workaround, you can store PrismaClient as a global variable in development environments only, as global variables are not reloaded:
 // src/lib/prisma.ts
 
 import { PrismaClient } from "@prisma/client";
@@ -18,4 +23,5 @@ if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
 
 // Resources
 // https://www.prisma.io/docs/orm/more/help-and-troubleshooting/nextjs-help
+// https://www.prisma.io/docs/orm/prisma-client/setup-and-configuration/databases-connections
 // https://www.robinwieruch.de/
